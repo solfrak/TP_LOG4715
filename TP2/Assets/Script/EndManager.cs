f2b62b3243fe88d1f38b60f54ca101018ac998fc
FelixLamarche <60437126+FelixLamarche@users.noreply.github.com>
2024-12-03 16:33:27 -0500

    Added End screen, replaced character at start

---
+++ TP2/Assets/Script/EndManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EndManager : MonoBehaviour
+{
+    [SerializeField]
+    CollisionChecker collisionChecker;
+
+    [SerializeField]
+    AudioClip endWinSFX;
+
+    [SerializeField]
+    Canvas endScreenCanvas;
+
+    bool isOver = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collisionChecker.IsInContact() && !isOver)
+        {
+            isOver = true;
+            FindAnyObjectByType<PlayerControler>()?.gameObject.SetActive(false);
+            FindAnyObjectByType<AudioManager>()?.PlaySFX(endWinSFX);
+            endScreenCanvas?.gameObject.SetActive(true);
+            Debug.Log("Game Over");
+        }
+    }
+}
